c2450755cbe1676f57472a14b21607497593cd95
Cozy Kuns <otwojoy@gmail.com>
2020-07-16 16:50:50 -0700

    create layout

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby '2.6.6'
 
 gem 'sinatra'
+gem 'haml'
 
 group :development do
   gem 'sinatra-reloader'
--- main.rb
+++ main.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'haml'
 
 get '/' do
-  return "Hello World!"
+  haml :index
+end
+
+post '/' do
+  @hand_string = params["hand"]
+  @call_string = params["calls"]
+  @dora_string = params["dora"]
+  @yaku_string = params["yaku"]
+
+  haml :result
 end
